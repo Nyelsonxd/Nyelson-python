175d7e9f68dc79611101d2c026b4f72220e31ca0
Your Name <you@example.com>
2026-04-13 20:18:35 -0300

    meu primeiro codigo

---
+++ naim.py
@@ -0,0 +1,12 @@
+print("hello word")
+
+variavel_1 = input("qual o seu nome? ")
+
+print(variavel_1)
+
+if variavel_1 == "Nyelson":
+    print("seu nome esta correto")
+elif variavel_1 == "Lima":
+    print("sobre nome esta correto")
+else:
+    print("nao e o seu nome")
